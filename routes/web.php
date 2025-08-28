8c8285d9b710c39c9411e2e8576718556022e407
Bima <flowgreen173@gmail.com>
2025-08-28 21:09:44 +0700

    Minor add

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,40 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/pencarian', function () {
+    return view('hasil_pencarian');
+});
+
+Route::get('/detail', function () {
+    return view('detail_kendaraan');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
+    Route::get('/moderasi', function () {
+        return view('admin.moderasi');
+    });
+
+    Route::get('/paket', function () {
+        return view('admin.paket');
+    });
+
+    Route::get('/userlist', function () {
+        return view('admin.pengguna');
+    });
 });
